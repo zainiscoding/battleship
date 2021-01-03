47b69af74e3e38313ee494dee06e66c73b714e7b
Zain <zainiscoding@gmail.com>
2021-01-03 22:05:52 +0900

    Began seperating vertical and horizontal ships

--- src/components/display_components/DisplayPlayerShipDock.jsx
+++ src/components/display_components/DisplayPlayerShipDock.jsx
@@ -11,65 +11,118 @@ const DisplayPlayerShipDock = (props) => {
           Click on the green area of a ship to begin placing. Click on the board
           to place the selected ship.
         </p>
+        <button
+          onClick={props.rotateShips}
+          className='ship__rotate-ship-button'
+        >
+          <img
+            className='rotate-ship-button__img'
+            src={rotateButton}
+            alt='Rotate ship icon'
+          />
+        </button>
       </div>
-      <button onClick={props.rotateShips} className='ship__rotate-ship-button'>
-        <img
-          className='rotate-ship-button__img'
-          src={rotateButton}
-          alt='Rotate ship icon'
-        />
-      </button>
       <div id='player-ships-wrapper__ships'>
-        {props.player.map((ship, index) => {
-          return (
-            <>
-              {ship.placed === false && (
-                <div
-                  className={
-                    ship.highlighted === false
-                      ? 'ship-highlight'
-                      : 'ship-highlight--selected'
-                  }
-                  data-shipnumber={index}
-                  data-length={ship.shipLength}
-                  data-orientation={ship.orientation}
-                  onClick={props.chooseShip}
-                >
+        <div id='player-ships-wrapper__ships__horizontal'>
+          {props.player.map((ship, index) => {
+            return (
+              <>
+                {ship.placed === false && ship.orientation === 'horizontal' && (
                   <div
-                    key={index}
                     className={
-                      'player-ships-wrapper__ship--' + ship.orientation
+                      ship.highlighted === false
+                        ? 'ship-highlight'
+                        : 'ship-highlight--selected'
                     }
                     data-shipnumber={index}
                     data-length={ship.shipLength}
                     data-orientation={ship.orientation}
                     onClick={props.chooseShip}
                   >
-                    <>
-                      {ship.positionsArray.map((position, index) => {
-                        return (
-                          <div
-                            key={index}
-                            className='player-ship-block--docked'
-                            id={index}
-                            data-x={props.setX(index)}
-                            data-y={props.setY(index)}
-                          ></div>
-                        );
-                      })}
-                    </>
+                    <div
+                      key={index}
+                      className={
+                        'player-ships-wrapper__ship--' + ship.orientation
+                      }
+                      data-shipnumber={index}
+                      data-length={ship.shipLength}
+                      data-orientation={ship.orientation}
+                      onClick={props.chooseShip}
+                    >
+                      <>
+                        {ship.positionsArray.map((position, index) => {
+                          return (
+                            <div
+                              key={index}
+                              className='player-ship-block--docked'
+                              id={index}
+                              data-x={props.setX(index)}
+                              data-y={props.setY(index)}
+                            ></div>
+                          );
+                        })}
+                      </>
+                    </div>
                   </div>
-                </div>
-              )}
-            </>
-          );
-        })}
+                )}
+              </>
+            );
+          })}
+        </div>
+        <div id='player-ships-wrapper__ships__vertical'>
+          {props.player.map((ship, index) => {
+            return (
+              <>
+                {ship.placed === false && ship.orientation === 'vertical' && (
+                  <div
+                    className={
+                      ship.highlighted === false
+                        ? 'ship-highlight'
+                        : 'ship-highlight--selected'
+                    }
+                    data-shipnumber={index}
+                    data-length={ship.shipLength}
+                    data-orientation={ship.orientation}
+                    onClick={props.chooseShip}
+                  >
+                    <div
+                      key={index}
+                      className={
+                        'player-ships-wrapper__ship--' + ship.orientation
+                      }
+                      data-shipnumber={index}
+                      data-length={ship.shipLength}
+                      data-orientation={ship.orientation}
+                      onClick={props.chooseShip}
+                    >
+                      <>
+                        {ship.positionsArray.map((position, index) => {
+                          return (
+                            <div
+                              key={index}
+                              className='player-ship-block--docked'
+                              id={index}
+                              data-x={props.setX(index)}
+                              data-y={props.setY(index)}
+                            ></div>
+                          );
+                        })}
+                      </>
+                    </div>
+                  </div>
+                )}
+              </>
+            );
+          })}
+        </div>
       </div>
-      <p>
-        Alternatively, press "auto place" to automatically place all ships and
-        begin the game.
-      </p>
-      <button onClick={props.placeRandomShips}>Auto place</button>
+      <div id='player_ships_wrapper--auto-place-wrapper'>
+        <p>
+          Alternatively, press "auto place" to automatically place all ships and
+          begin the game.
+        </p>
+        <button onClick={props.placeRandomShips}>Auto place</button>
+      </div>
     </div>
   );
 };
